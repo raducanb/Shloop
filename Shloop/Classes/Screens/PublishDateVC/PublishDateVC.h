10435c724f84c13d334080c209467eb561aa67cf
raducanb <bogdan@Mac03s-Mac-mini.local>
2014-10-08 16:08:19 +0300

    Added PublishDateVC

---
+++ Shloop/Classes/Screens/PublishDateVC/PublishDateVC.h
@@ -0,0 +1,13 @@
+//
+//  PublishDateVC.h
+//  Shloop
+//
+//  Created by Bogdan on 08/10/14.
+//  Copyright (c) 2014 Bogdan Raducan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PublishDateVC : UIViewController
+
+@end
---
+++ Shloop/Classes/Screens/PublishDateVC/PublishDateVC.m
@@ -0,0 +1,36 @@
+//
+//  PublishDateVC.m
+//  Shloop
+//
+//  Created by Bogdan on 08/10/14.
+//  Copyright (c) 2014 Bogdan Raducan. All rights reserved.
+//
+
+#import "PublishDateVC.h"
+
+@interface PublishDateVC ()
+
+@end
+
+@implementation PublishDateVC
+
+#pragma mark - Life cycle
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
